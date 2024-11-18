1c4d40c1011adc5c27ae29b76150ac71c8f2b7f8
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2024-11-18 12:26:39 -0800

    Create Lesson.tsx

---
+++ frontend/src/views/Lesson.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+    const Lesson: React.FC = () => {
+      return (
+        <div>
+          <h1>Lesson Page</h1>
+          <p>This is where you will find detailed lessons, quizzes, and an interactive Python code editor.</p>
+        </div>
+      );
+    };
+
+    export default Lesson;

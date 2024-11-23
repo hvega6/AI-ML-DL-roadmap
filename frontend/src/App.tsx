c78de0ddfec57007425c0f72f0a3632a6b710044
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2024-11-23 13:19:42 -0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import { ParticleProvider } from './context/ParticleContext';
 import PrivateRoute from './components/PrivateRoute';
+import Navigation from './components/Navigation';
 import Home from './views/Home';
 import Login from './views/Login';
+import Register from './views/Register';
 import Dashboard from './views/Dashboard';
-import Navigation from './components/Navigation';
-import Lesson from './views/Lesson';
 import Lessons from './views/Lessons';
+import Lesson from './views/Lesson';
 import Curriculum from './views/Curriculum';
 import Resources from './views/Resources';
+import Profile from './views/Profile';
 
 const App: React.FC = () => {
   return (
@@ -25,6 +27,7 @@ const App: React.FC = () => {
                 {/* Public Routes */}
                 <Route exact path="/" component={Home} />
                 <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
                 <Route path="/curriculum" component={Curriculum} />
                 <Route path="/resources" component={Resources} />
                 
@@ -32,6 +35,12 @@ const App: React.FC = () => {
                 <PrivateRoute path="/dashboard" component={Dashboard} />
                 <PrivateRoute exact path="/lessons" component={Lessons} />
                 <PrivateRoute path="/lesson/:id" component={Lesson} />
+                <PrivateRoute path="/profile" component={Profile} />
+                
+                {/* Catch all route */}
+                <Route path="*">
+                  <Redirect to="/" />
+                </Route>
               </Switch>
             </div>
           </AuthProvider>

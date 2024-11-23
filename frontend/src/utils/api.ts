bfa36f1f2057bd5db55ac4112013e8fb6073349b
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2024-11-22 23:30:20 -0800

    Update api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import authService from '../services/authService';
 
+const BASE_URL = import.meta.env.VITE_API_URL?.replace('/auth', '');
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
